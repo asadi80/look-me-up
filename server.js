c816da5c4221a402a5f07eec3b3efe84c079adf4
amarten <alexdmarten@gmail.com>
2022-02-10 21:22:38 -0800

    resolve conflict

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
